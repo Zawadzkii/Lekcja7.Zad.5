b278792c5c014463f379fc4a990970f940db9668
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-13 07:02:04 +0200

    Add project files.

---
+++ Lekcja7.Zad.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Podaj swój wiek: ");
+
+		// Pobranie wieku od użytkownika i konwersja na typ int
+		int wiek = Convert.ToInt32(Console.ReadLine());
+
+		// Sprawdzenie warunków dla różnych stanowisk
+		if (wiek >= 21)
+		{
+			Console.WriteLine("Możesz zostać posłem.");
+		}
+		if (wiek >= 35)
+		{
+			Console.WriteLine("Możesz zostać premierem lub senatorem.");
+		}
+		if (wiek >= 35)
+		{
+			Console.WriteLine("Możesz zostać prezydentem.");
+		}
+
+		Console.ReadLine();
+	}
+}
